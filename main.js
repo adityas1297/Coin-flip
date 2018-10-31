dd57b0524dd361385a5139eca61eaefbaacf438b
Aditya Singh <adityasingh1297@gmail.com>
2018-10-31 22:46:46 +0530

    Added delay

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function flipCoin() {
+	document.getElementById('flipResult').innerHTML = "Flipping...";
+	setTimeout(actualFlipping, 2000);
+}
+function actualFlipping() {
+	// body...
 	var result = Math.random();
 	if(result <= 0.5)
 		document.getElementById('flipResult').innerHTML = "HEAD";
